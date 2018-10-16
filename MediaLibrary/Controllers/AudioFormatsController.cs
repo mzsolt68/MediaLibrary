3de79153a6c2ee71aded238f53ce574b74080971
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-10-16 23:58:15 +0200

    Adding controllers and views for Albums, Audio Formats, Performers and Songs

---
+++ MediaLibrary/Controllers/AudioFormatsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MediaLibrary.Data;
+using MediaLibrary.Models.Audios;
+
+namespace MediaLibrary.Controllers
+{
+    public class AudioFormatsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AudioFormatsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AudioFormats
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AudioFormats.ToListAsync());
+        }
+
+        // GET: AudioFormats/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var audioFormat = await _context.AudioFormats
+                .FirstOrDefaultAsync(m => m.AudioFormatID == id);
+            if (audioFormat == null)
+            {
+                return NotFound();
+            }
+
+            return View(audioFormat);
+        }
+
+        // GET: AudioFormats/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AudioFormats/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AudioFormatID,AudioFormatName")] AudioFormat audioFormat)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(audioFormat);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(audioFormat);
+        }
+
+        // GET: AudioFormats/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var audioFormat = await _context.AudioFormats.FindAsync(id);
+            if (audioFormat == null)
+            {
+                return NotFound();
+            }
+            return View(audioFormat);
+        }
+
+        // POST: AudioFormats/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AudioFormatID,AudioFormatName")] AudioFormat audioFormat)
+        {
+            if (id != audioFormat.AudioFormatID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(audioFormat);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AudioFormatExists(audioFormat.AudioFormatID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(audioFormat);
+        }
+
+        // GET: AudioFormats/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var audioFormat = await _context.AudioFormats
+                .FirstOrDefaultAsync(m => m.AudioFormatID == id);
+            if (audioFormat == null)
+            {
+                return NotFound();
+            }
+
+            return View(audioFormat);
+        }
+
+        // POST: AudioFormats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var audioFormat = await _context.AudioFormats.FindAsync(id);
+            _context.AudioFormats.Remove(audioFormat);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AudioFormatExists(int id)
+        {
+            return _context.AudioFormats.Any(e => e.AudioFormatID == id);
+        }
+    }
+}
---
+++ MediaLibrary/Controllers/PerformersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MediaLibrary.Data;
+using MediaLibrary.Models.Audios;
+
+namespace MediaLibrary.Controllers
+{
+    public class PerformersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PerformersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Performers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Performers.ToListAsync());
+        }
+
+        // GET: Performers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var performer = await _context.Performers
+                .FirstOrDefaultAsync(m => m.PerformerID == id);
+            if (performer == null)
+            {
+                return NotFound();
+            }
+
+            return View(performer);
+        }
+
+        // GET: Performers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Performers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PerformerID,PerformerName")] Performer performer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(performer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(performer);
+        }
+
+        // GET: Performers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var performer = await _context.Performers.FindAsync(id);
+            if (performer == null)
+            {
+                return NotFound();
+            }
+            return View(performer);
+        }
+
+        // POST: Performers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PerformerID,PerformerName")] Performer performer)
+        {
+            if (id != performer.PerformerID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(performer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PerformerExists(performer.PerformerID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(performer);
+        }
+
+        // GET: Performers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var performer = await _context.Performers
+                .FirstOrDefaultAsync(m => m.PerformerID == id);
+            if (performer == null)
+            {
+                return NotFound();
+            }
+
+            return View(performer);
+        }
+
+        // POST: Performers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var performer = await _context.Performers.FindAsync(id);
+            _context.Performers.Remove(performer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PerformerExists(int id)
+        {
+            return _context.Performers.Any(e => e.PerformerID == id);
+        }
+    }
+}
