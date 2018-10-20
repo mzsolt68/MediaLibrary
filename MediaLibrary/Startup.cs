9c2d0eb77cb6f2fc5aefd89fb43131ce127bccc9
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-10-20 21:19:19 +0200

    Change default startpage to Audio Library index page

--- MediaLibrary/Startup.cs
+++ MediaLibrary/Startup.cs
@@ -74,7 +74,7 @@ namespace MediaLibrary
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=AudioHome}/{action=Index}/{id?}");
             });
         }
     }
