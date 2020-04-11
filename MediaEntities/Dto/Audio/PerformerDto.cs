39a20997d35f5f443ffdc34075eeca47d7954ec7
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2020-04-11 11:44:26 +0200

    Add new DTO-s to transfer song and performer data from backend.

---
+++ MediaEntities/Dto/Audio/PerformerDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaLibrary.Entities.Dto.Audio
+{
+    public class PerformerDto
+    {
+        public int PerformerID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MediaEntities/Dto/Audio/SongDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaLibrary.Entities.Dto.Audio
+{
+    public class SongDto
+    {
+        public int SongID { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<PerformerDto> Performers { get; set; }
+    }
+}
