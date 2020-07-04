302313cc39efa3d97f9aa5d42f6206c4082ee733
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2020-07-04 21:35:04 +0200

    Added GenreRepository interface

---
+++ MediaLibrary.Common/Interfaces/Common/IGenreRepository.cs
@@ -0,0 +1,18 @@
+﻿using MediaLibrary.Entities.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaLibrary.Common.Interfaces.Common
+{
+    interface IGenreRepository
+    {
+        Task<Genre> AddGenre(Genre newGenre);
+        Task<int> DeleteGenre(int? id);
+        Task<Genre> UpdateGenre(Genre updatedGenre);
+        Task<ICollection<Genre>> GetGenres();
+        Task<ICollection<Genre>> GetAudioGenres();
+        Task<ICollection<Genre>> GetVideoGenres();
+    }
+}
