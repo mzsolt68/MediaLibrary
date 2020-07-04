5f29978d538e2f9d8eb5c664ec78a8e69a885c95
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2020-07-04 21:28:15 +0200

    Add API calls to GenreController

---
+++ MediaLibrary.MediaApi/Controllers/Common/GenreController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaLibrary.Common.Interfaces.Services;
+using MediaLibrary.Entities.Models.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaLibrary.MediaApi.Controllers.Common
+{
+    [Route("api/common/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly ICommonService _service;
+
+        public GenreController(ICommonService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("getgenres")]
+        public async Task<ActionResult<ICollection<Genre>>> GetGenres()
+        {
+            var result = await _service.GetGenres();
+            if(result == null)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }
+
+        [HttpGet("getaudiogenres")]
+        public async Task<ActionResult<ICollection<Genre>>> GetAudioGenres()
+        {
+            var result = await _service.GetAudioGenres();
+            if(result == null)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }
+
+        [HttpGet("getvideogenres")]
+        public async Task<ActionResult<ICollection<Genre>>> GetVideoGenres()
+        {
+            var result = await _service.GetVideoGenres();
+            if(result == null)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("addgenre")]
+        public async Task<ActionResult<Genre>> AddGenre([FromBody] Genre newGenre)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var result = await _service.AddGenre(newGenre);
+            if(result == null)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete("deletegenre/{id}")]
+        public async Task<ActionResult> DeleteGenre(int? id)
+        {
+            if(!id.HasValue)
+            {
+                return BadRequest();
+            }
+            if(await _service.DeleteGenre(id) == 0)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [HttpPut("updategenre")]
+        public async Task<ActionResult<Genre>> UpdateGenre([FromBody] Genre updatedGenre)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var result = await _service.UpdateGenre(updatedGenre);
+            if(result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
