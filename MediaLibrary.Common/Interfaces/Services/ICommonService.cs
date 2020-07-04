2e30284c17c2cc8e38e2db626158a37b3e2e3d6c
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2020-07-04 20:59:55 +0200

    CommonService interface added

---
+++ MediaLibrary.Common/Interfaces/Services/ICommonService.cs
@@ -0,0 +1,29 @@
+﻿using MediaLibrary.Entities.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaLibrary.Common.Interfaces.Services
+{
+    public interface ICommonService
+    {
+        Task<Genre> AddGenre(Genre newGenre);
+        Task<int> DeleteGenre(int? id);
+        Task<Genre> UpdateGenre(Genre updatedGenre);
+        Task<ICollection<Genre>> GetGenres();
+        Task<ICollection<Genre>> GetAudioGenres();
+        Task<ICollection<Genre>> GetVideoGenres();
+
+        Task<Language> AddLanguage(Language newLanguage);
+        Task<int> DeleteLanguage(int? id);
+        Task<Language> UpdateLanguage(Language updatedLanguage);
+        Task<ICollection<Language>> GetLanguages();
+
+        Task<Tag> AddTag(Tag newTag);
+        Task<int> DeleteTag(int? id);
+        Task<Tag> UpdateTag(Tag updatedTag);
+        Task<ICollection<Tag>> GetTags();
+
+    }
+}
