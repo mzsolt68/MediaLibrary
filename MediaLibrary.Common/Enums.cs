c5e77c536aab385e89e85d38031bead90ffd616b
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2021-08-08 19:50:39 +0200

    Add Enums to later use.

---
+++ MediaLibrary.Common/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaLibrary.Common
+{
+    public class Enums
+    {
+        public enum GenreType
+        {
+            none,
+            audio,
+            video
+        }
+    }
+}
