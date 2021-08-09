746d816e264c927a7d5a435b2714c4902edb45fd
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2021-08-09 21:41:44 +0200

    Add repository skeletons

---
+++ NediaLibrary.Repositories/Books/AuthorRepository.cs
@@ -0,0 +1,8 @@
+﻿using MediaLibrary.Common.Interfaces.Books;
+
+namespace MediaLibrary.Repositories.Books
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+    }
+}
---
+++ NediaLibrary.Repositories/Books/BookRepository.cs
@@ -0,0 +1,8 @@
+﻿using MediaLibrary.Common.Interfaces.Books;
+
+namespace MediaLibrary.Repositories.Books
+{
+    public class BookRepository : IBookRepository
+    {
+    }
+}
