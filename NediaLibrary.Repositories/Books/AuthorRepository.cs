52dba2e989f8220d830e51111309feb697eb5262
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2021-08-09 21:50:58 +0200

    Add constructor and DB context to repositories

--- NediaLibrary.Repositories/Books/AuthorRepository.cs
+++ NediaLibrary.Repositories/Books/AuthorRepository.cs
@@ -1,8 +1,15 @@
 ﻿using MediaLibrary.Common.Interfaces.Books;
+using MediaLibrary.Entities.Data;
 
 namespace MediaLibrary.Repositories.Books
 {
     public class AuthorRepository : IAuthorRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
     }
 }
--- NediaLibrary.Repositories/Books/BookRepository.cs
+++ NediaLibrary.Repositories/Books/BookRepository.cs
@@ -1,8 +1,15 @@
 ﻿using MediaLibrary.Common.Interfaces.Books;
+using MediaLibrary.Entities.Data;
 
 namespace MediaLibrary.Repositories.Books
 {
     public class BookRepository : IBookRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
     }
 }
