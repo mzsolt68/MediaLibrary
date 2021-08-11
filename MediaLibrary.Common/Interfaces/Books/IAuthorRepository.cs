39c03877b901a32f67940ee8895193e8ee60fdc3
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2021-08-11 20:49:58 +0200

    Implement book's Author repository base

--- MediaLibrary.Common/Interfaces/Books/IAuthorRepository.cs
+++ MediaLibrary.Common/Interfaces/Books/IAuthorRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace MediaLibrary.Common.Interfaces.Books
+﻿using MediaLibrary.Entities.Models.Books;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MediaLibrary.Common.Interfaces.Books
 {
     public interface IAuthorRepository
     {
+        Task<Author> AddAuthor(Author newAuthor);
+        Task<Author> UpdateAuthor(Author updatedAuthor);
+        Task<int> DeleteAuthor(int? authorID);
+        Task<Author> GetAuthorByID(int? authorID);
+        Task<ICollection<Author>> GetAuthors();
+        Task<ICollection<Book>> GetBooksOfAuthor(int? authorID);
     }
 }
