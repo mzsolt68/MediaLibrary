e3c1849e8c1e19885958571f1a97ca24f8808f4e
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2021-08-19 21:08:39 +0200

    Implement BookRepository interface

--- MediaLibrary.Common/Interfaces/Books/IBookRepository.cs
+++ MediaLibrary.Common/Interfaces/Books/IBookRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace MediaLibrary.Common.Interfaces.Books
+﻿using MediaLibrary.Entities.Models.Books;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MediaLibrary.Common.Interfaces.Books
 {
     public interface IBookRepository
     {
+        Task<Book> AddBook(Book newBook, ICollection<int> authorIDs, ICollection<int> formatIDs, ICollection<int> tagIDs);
+        Task<int> DeleteBook(int? bookID);
+        Task<Book> UpdateBook(Book updatedBook, ICollection<int> authorIDs, ICollection<int> formatIDs, ICollection<int> tagIDs);
+        Task<ICollection<Book>> GetBooks();
+        Task<Book> GetBookByID(int? bookID);
+        Task<ICollection<Book>> GetBooksByFormat(int? formatID);
+        Task<ICollection<Book>> GetBooksByTag(int? tagID);
+        Task<ICollection<Book>> GetBooksOfPublisher(int? publisherID);
     }
 }
