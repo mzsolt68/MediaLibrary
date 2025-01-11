444835727949e665275b7b49d6bfc355516e56e7
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-01-11 17:44:15 +0100

    Add Error Type and Error classes

---
+++ SharedKernel/ErrorType.cs
@@ -0,0 +1,14 @@
+﻿namespace SharedKernel
+{
+    /// <summary>
+    /// Error type
+    /// </summary>
+    public enum ErrorType
+    {
+        Failure = 0,
+        Validatiuons = 1,
+        Problem = 2,
+        NotFound = 3,
+        Conflict = 4
+    }
+}
