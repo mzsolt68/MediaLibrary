ca0e7d90ec1280037de66b649911b51b5288ff72
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-01-11 20:36:45 +0100

    Update propertty setters

--- src/Domain/Models/Audio/AlbumSong.cs
+++ src/Domain/Models/Audio/AlbumSong.cs
@@ -18,21 +18,21 @@ namespace Domain.Models.Audio
         }
 
         [Required]
-        public Guid AlbumID { get; set; }
+        public Guid AlbumID { get; private set; }
         [JsonIgnore]
-        public Album Album { get; set; }
+        public Album Album { get; private set; }
         [Required]
-        public Guid SongID { get; set; }
-        public Song Song { get; set; }
+        public Guid SongID { get; private set; }
+        public Song Song { get; private set; }
         [Required]
-        public int TrackNr { get; set; }
+        public int TrackNr { get; private set; }
         [Required]
         [Display(Name = "Játékidő")]
-        public string PlayTime { get; set; }
+        public string PlayTime { get; private set; }
         [Display(Name = "Lemez")]
-        public Byte Disc { get; set; }
+        public Byte Disc { get; private set; }
         [Display(Name = "Megjegyzés")]
-        public string Note { get; set; }
+        public string Note { get; private set; }
 
         public static AlbumSong Create(Guid albumID, Guid songID, int trackNr, string playTime, Byte disc, string note)
         {
