e2298efe34f4709955e15ee9f136c66fdd09a93f
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-01-11 21:45:05 +0100

    Adding test cases to Genre entity

--- src/Domain/Models/Common/Genre.cs
+++ src/Domain/Models/Common/Genre.cs
@@ -18,11 +18,19 @@ namespace Domain.Models.Common
         [Required]
         public string GenreType { get; private set; }
 
-        public static Genre Create(string genreName, string genreType)
+        public static Result<Genre> Create(string genreName, string genreType)
         {
+            if(string.IsNullOrWhiteSpace(genreName))
+            {
+                return Result.Failure<Genre>(new Error("GenreName.Missing", "Genre name is missing", ErrorType.Validation));
+            }
+            if (string.IsNullOrWhiteSpace(genreType))
+            {
+                return Result.Failure<Genre>(new Error("GenreType.Missing", "Genre type is missing", ErrorType.Validation));
+            }
             var genre = new Genre(Guid.NewGuid(), genreName, genreType);
             genre.IsActive = true;
-            return genre;
+            return Result.Success(genre);
         }
 
         public void Update(string genreName, string genreType)
---
+++ tests/Domain.UnitTests/Common/GenreTests.cs
@@ -0,0 +1,84 @@
+﻿using Domain.Models.Common;
+using SharedKernel;
+using Shouldly;
+
+
+namespace Domain.UnitTests.Common
+{
+    public class GenreTests
+    {
+        [Fact]
+        public void ProperParametersShouldReturnGenre()
+        {
+            // Arrange
+            var genreName = "Horror";
+            var genreType = "Scary";
+            // Act
+            var result = Genre.Create(genreName, genreType);
+            // Assert
+            result.ShouldNotBeNull();
+            result.IsFailure.ShouldBeFalse();
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.GenreName.ShouldBe(genreName);
+            result.Value.GenreType.ShouldBe(genreType);
+            result.Value.ShouldNotBeNull();
+            result.Value.IsActive.ShouldBeTrue();
+            result.Value.CreatedAt.ShouldBeLessThanOrEqualTo(DateTime.UtcNow);
+            result.Value.UpdatedAt.ShouldBeLessThanOrEqualTo(DateTime.UtcNow);
+            result.Value.Id.ShouldNotBe(Guid.Empty);
+        }
+
+        [Fact]
+        public void EmptyGenreNameShouldReturnError()
+        {
+            // Arrange
+            var genreName = "";
+            var genreType = "Scary";
+            // Act
+            var result = Genre.Create(genreName, genreType);
+            // Assert
+            result.ShouldNotBeNull();
+            result.IsFailure.ShouldBeTrue();
+            result.IsSuccess.ShouldBeFalse();
+            result.Error.Type.ShouldBe(ErrorType.Validation);
+            result.Error.Code.ShouldBe("GenreName.Missing");
+            result.Error.Message.ShouldBe("Genre name is missing");
+        }
+
+        [Fact]
+        public void EmptyGenreTypeShouldReturnError()
+        {
+            // Arrange
+            var genreName = "Horror";
+            var genreType = "";
+            // Act
+            var result = Genre.Create(genreName, genreType);
+            // Assert
+            result.ShouldNotBeNull();
+            result.IsFailure.ShouldBeTrue();
+            result.IsSuccess.ShouldBeFalse();
+            result.Error.Type.ShouldBe(ErrorType.Validation);
+            result.Error.Code.ShouldBe("GenreType.Missing");
+            result.Error.Message.ShouldBe("Genre type is missing");
+        }
+
+        [Fact]
+        public void UpdateShouldChangeGenreNameAndGenreType()
+        {
+            // Arrange
+            var genreName = "Horror";
+            var genreType = "Scary";
+            var genre = Genre.Create(genreName, genreType).Value;
+            var newGenreName = "Comedy";
+            var newGenreType = "Funny";
+            // Act
+            genre.Update(newGenreName, newGenreType);
+            // Assert
+            genre.ShouldNotBeNull();
+            genre.IsActive.ShouldBeTrue();
+            genre.GenreName.ShouldBe(newGenreName);
+            genre.GenreType.ShouldBe(newGenreType);
+            genre.UpdatedAt.ShouldBeLessThanOrEqualTo(DateTime.UtcNow);
+        }
+    }
+}
