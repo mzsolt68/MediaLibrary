f93111ad103ce5c08e430b79772c3dcc1aaa6158
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-01-11 19:40:37 +0100

    Add new properties to Entity base class

--- src/SharedKernel/Entity.cs
+++ src/SharedKernel/Entity.cs
@@ -5,6 +5,17 @@
         private readonly List<IDomainEvent> _domainEvents = [];
         public List<IDomainEvent> DomainEvents => _domainEvents;
 
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime UpdatedAt { get; protected set; }
+
+        public bool IsActive { get; protected set; }
+
+        protected Entity(Guid id)
+        {
+            Id = id;
+        }
+
         public void ClearDomainEvents()
         {
             _domainEvents.Clear();
@@ -13,6 +24,12 @@
         public void Raise(IDomainEvent domainEvent)
         {
             _domainEvents.Add(domainEvent);
+        }
+
+        public void SetActiveState(bool newState)
+        {
+            IsActive = newState;
+            UpdatedAt = DateTime.UtcNow;
         }
     }
 }
