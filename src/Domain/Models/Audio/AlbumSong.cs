f88223350aa1bae5bcf885220ae4da9b12d96e54
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-01-21 20:20:04 +0100

    Add unit tests to AlbumSong entity

--- src/Domain/Models/Audio/AlbumSong.cs
+++ src/Domain/Models/Audio/AlbumSong.cs
@@ -34,11 +34,31 @@ namespace Domain.Models.Audio
         [Display(Name = "Megjegyzés")]
         public string Note { get; private set; }
 
-        public static AlbumSong Create(Guid albumID, Guid songID, int trackNr, string playTime, Byte disc, string note)
+        public static Result<AlbumSong> Create(Guid albumID, Guid songID, int trackNr, string playTime, Byte disc, string note)
         {
+            if(string.IsNullOrWhiteSpace(playTime))
+            {
+                return Result.Failure<AlbumSong>(new Error("PlayTime.Missing", "Play time is missing", ErrorType.Validation));
+            }
+            if (albumID == Guid.Empty)
+            {
+                return Result.Failure<AlbumSong>(new Error("AlbumID.Missing", "Album ID is missing", ErrorType.Validation));
+            }
+            if (songID == Guid.Empty)
+            {
+                return Result.Failure<AlbumSong>(new Error("SongID.Missing", "Song ID is missing", ErrorType.Validation));
+            }
+            if(trackNr < 1)
+            {
+                return Result.Failure<AlbumSong>(new Error("TrackNr.Invalid", "Track number is invalid", ErrorType.Validation));
+            }
+            if(disc < 1)
+            {
+                return Result.Failure<AlbumSong>(new Error("Disc.Invalid", "Disc number is invalid", ErrorType.Validation));
+            }
             var albumSong = new AlbumSong(Guid.NewGuid(), albumID, songID, trackNr, playTime, disc, note);
             albumSong.IsActive = true;
-            return albumSong;
+            return Result.Success(albumSong);
         }
     }
 }
---
+++ tests/Domain.UnitTests/Audio/AlbumSongTests.cs
@@ -0,0 +1,168 @@
+﻿using Shouldly;
+using Domain.Models.Audio;
+using SharedKernel;
+
+namespace Domain.UnitTests.Audio
+{
+    /// <summary>
+    /// Unit tests for <see cref="AlbumSong"/> entity.
+    /// </summary>
+    public class AlbumSongTests
+    {
+        /// <summary>
+        /// Proper parameters should return <see cref="AlbumSong"/>.
+        /// </summary>
+        [Fact]
+        public void ProperParametersShouldReturnAlbumSong()
+        {
+            // Arrange
+            var albumID = Guid.NewGuid();
+            var songID = Guid.NewGuid();
+            var trackNr = 1;
+            var playTime = "3:45";
+            byte disc = 1;
+            var note = "Note";
+            // Act
+            var result = AlbumSong.Create(albumID, songID, trackNr, playTime, disc, note);
+            // Assert
+            result.ShouldNotBeNull();
+            result.IsFailure.ShouldBeFalse();
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.AlbumID.ShouldBe(albumID);
+            result.Value.SongID.ShouldBe(songID);
+            result.Value.TrackNr.ShouldBe(trackNr);
+            result.Value.PlayTime.ShouldBe(playTime);
+            result.Value.Disc.ShouldBe(disc);
+            result.Value.Note.ShouldBe(note);
+            result.Value.ShouldNotBeNull();
+            result.Value.IsActive.ShouldBeTrue();
+            result.Value.CreatedAt.ShouldBeLessThanOrEqualTo(DateTime.UtcNow);
+            result.Value.UpdatedAt.ShouldBeLessThanOrEqualTo(DateTime.UtcNow);
+            result.Value.Id.ShouldNotBe(Guid.Empty);
+        }
+        /// <summary>
+        /// Empty play time should return error.
+        /// <see cref="AlbumSong.Create(Guid, Guid, int, string, byte, string)"/>
+        /// </summary>
+        [Fact]
+        public void EmptyPlayTimeShouldReturnError()
+        {
+            // Arrange
+            var albumID = Guid.NewGuid();
+            var songID = Guid.NewGuid();
+            var trackNr = 1;
+            var playTime = "";
+            byte disc = 1;
+            var note = "Note";
+            // Act
+            var result = AlbumSong.Create(albumID, songID, trackNr, playTime, disc, note);
+            // Assert
+            result.ShouldNotBeNull();
+            result.IsFailure.ShouldBeTrue();
+            result.IsSuccess.ShouldBeFalse();
+            result.Error.Type.ShouldBe(ErrorType.Validation);
+            result.Error.Code.ShouldBe("PlayTime.Missing");
+            result.Error.Message.ShouldBe("Play time is missing");
+        }
+
+        /// <summary>
+        /// Empty album ID should return error.
+        /// <see cref="AlbumSong.Create(Guid, Guid, int, string, byte, string)"/>
+        /// </summary>
+        [Fact]
+        public void EmptyAlbumIDShouldReturnError()
+        {
+            // Arrange
+            var albumID = Guid.Empty;
+            var songID = Guid.NewGuid();
+            var trackNr = 1;
+            var playTime = "3:45";
+            byte disc = 1;
+            var note = "Note";
+            // Act
+            var result = AlbumSong.Create(albumID, songID, trackNr, playTime, disc, note);
+            // Assert
+            result.ShouldNotBeNull();
+            result.IsFailure.ShouldBeTrue();
+            result.IsSuccess.ShouldBeFalse();
+            result.Error.Type.ShouldBe(ErrorType.Validation);
+            result.Error.Code.ShouldBe("AlbumID.Missing");
+            result.Error.Message.ShouldBe("Album ID is missing");
+        }
+
+        /// <summary>
+        /// Empty song ID should return error.
+        /// <see cref="AlbumSong.Create(Guid, Guid, int, string, byte, string)"/>
+        /// </summary>
+        [Fact]
+        public void EmptySongIDShouldReturnError()
+        {
+            // Arrange
+            var albumID = Guid.NewGuid();
+            var songID = Guid.Empty;
+            var trackNr = 1;
+            var playTime = "3:45";
+            byte disc = 1;
+            var note = "Note";
+            // Act
+            var result = AlbumSong.Create(albumID, songID, trackNr, playTime, disc, note);
+            // Assert
+            result.ShouldNotBeNull();
+            result.IsFailure.ShouldBeTrue();
+            result.IsSuccess.ShouldBeFalse();
+            result.Error.Type.ShouldBe(ErrorType.Validation);
+            result.Error.Code.ShouldBe("SongID.Missing");
+            result.Error.Message.ShouldBe("Song ID is missing");
+        }
+
+        /// <summary>   
+        /// Empty track number should return error.
+        /// <see cref="AlbumSong.Create(Guid, Guid, int, string, byte, string)"/>
+        /// </summary>
+        [Fact]
+        public void EmptyTrackNumberShouldReturnError()
+        {
+            // Arrange
+            var albumID = Guid.NewGuid();
+            var songID = Guid.NewGuid();
+            var trackNr = 0;
+            var playTime = "3:45";
+            byte disc = 1;
+            var note = "Note";
+            // Act
+            var result = AlbumSong.Create(albumID, songID, trackNr, playTime, disc, note);
+            // Assert
+            result.ShouldNotBeNull();
+            result.IsFailure.ShouldBeTrue();
+            result.IsSuccess.ShouldBeFalse();
+            result.Error.Type.ShouldBe(ErrorType.Validation);
+            result.Error.Code.ShouldBe("TrackNr.Invalid");
+            result.Error.Message.ShouldBe("Track number is invalid");
+        }
+
+        /// <summary>
+        /// Empty disc should return error.
+        /// <see cref="AlbumSong.Create(Guid, Guid, int, string, byte, string)"/>
+        /// </summary>
+        [Fact]
+        public void EmptyDiscShouldReturnError()
+        {
+            // Arrange
+            var albumID = Guid.NewGuid();
+            var songID = Guid.NewGuid();
+            var trackNr = 1;
+            var playTime = "3:45";
+            byte disc = 0;
+            var note = "Note";
+            // Act
+            var result = AlbumSong.Create(albumID, songID, trackNr, playTime, disc, note);
+            // Assert
+            result.ShouldNotBeNull();
+            result.IsFailure.ShouldBeTrue();
+            result.IsSuccess.ShouldBeFalse();
+            result.Error.Type.ShouldBe(ErrorType.Validation);
+            result.Error.Code.ShouldBe("Disc.Invalid");
+            result.Error.Message.ShouldBe("Disc number is invalid");
+        }
+    }
+}
