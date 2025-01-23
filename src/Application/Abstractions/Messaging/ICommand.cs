86ae733476148255b85e5e4ab7037a7190e2e54d
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-01-23 20:35:23 +0100

    Add command and query interfaces

---
+++ src/Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using SharedKernel;
+
+namespace Application.Abstractions.Messaging
+{
+    public interface ICommand : IRequest<Result>, IBaseCommand
+    {
+    }
+
+    public interface ICommand<TResponse> : IRequest<Result<TResponse>>, IBaseCommand
+    {
+    }
+
+    public interface IBaseCommand
+    {
+    }
+}
---
+++ src/Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using SharedKernel;
+
+namespace Application.Abstractions.Messaging
+{
+    public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+    {
+    }
+}
