90d52073739cb49a673059a9c760ca9409d92b7b
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-01-23 20:51:23 +0100

    Add command hadler skeleton to create Genre

---
+++ src/Application/Common/Genre/Create/CreateGenreCommand.cs
@@ -0,0 +1,12 @@
+﻿
+using Application.Abstractions.Messaging;
+using System.Windows.Input;
+
+namespace Application.Common.Genre.Create
+{
+    public sealed class CreateGenreCommand : ICommand<Guid>
+    {
+        public string GenreName { get; set; }
+        public string GenreType { get; set; }
+    }
+}
---
+++ src/Application/Common/Genre/Create/CreateGenreCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿
+using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using SharedKernel;
+
+namespace Application.Common.Genre.Create
+{
+    internal sealed class CreateGenreCommandHandler(IApplicationDbContext context) : ICommandHandler<CreateGenreCommand, Guid>
+    {
+        public async Task<Result<Guid>> Handle(CreateGenreCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
