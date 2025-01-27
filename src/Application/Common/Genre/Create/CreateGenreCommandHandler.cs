c5857e88f0f28a3f8884cc0706b95626ad59c85f
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-01-27 20:07:01 +0100

    Implement create genre command and unit tests

--- src/Application/Common/Genre/Create/CreateGenreCommandHandler.cs
+++ src/Application/Common/Genre/Create/CreateGenreCommandHandler.cs
@@ -2,6 +2,7 @@
 using Application.Abstractions.Data;
 using Application.Abstractions.Messaging;
 using SharedKernel;
+using Domain.Models.Common;
 
 namespace Application.Common.Genre.Create
 {
@@ -9,7 +10,14 @@ namespace Application.Common.Genre.Create
     {
         public async Task<Result<Guid>> Handle(CreateGenreCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var genreResult = Domain.Models.Common.Genre.Create(request.GenreName, request.GenreType);
+            if(genreResult.IsFailure)
+            {
+                return Result.Failure<Guid>(new Error(genreResult.Error.Code, genreResult.Error.Message, genreResult.Error.Type));
+            }
+            context.Genres.Add(genreResult.Value);
+            await context.SaveChangesAsync(cancellationToken);
+            return Result.Success(genreResult.Value.Id);
         }
     }
 }
---
+++ tests/Application.UnitTests/Common/CreateGenreCommandTests.cs
@@ -0,0 +1,95 @@
+﻿using Shouldly;
+using Moq;
+using Application.Common.Genre.Create;
+using Application.Abstractions.Data;
+using Microsoft.EntityFrameworkCore;
+using Domain.Models.Common;
+using SharedKernel;
+
+namespace Application.UnitTests.Common
+{
+    /// <summary>
+    /// Unit tests for the CreateGenreCommand class.
+    /// </summary>
+    public class CreateGenreCommandTests
+    {
+        private readonly Mock<IApplicationDbContext> _context;
+
+        public CreateGenreCommandTests()
+        {
+            _context = new Mock<IApplicationDbContext>();
+            _context.Setup(x=> x.Genres).Returns(new Mock<DbSet<Genre>>().Object);
+        }
+
+        /// <summary>
+        /// Proper parameters should create a new genre
+        /// <see cref="CreateGenreCommand"/>
+        /// </summary>
+        [Fact]
+        public async Task ProperParametersShouldCreateNewGenre()
+        {
+            // Arrange
+            var genreName = "Test Genre";
+            var genreType = "Test Type";
+            var command = new CreateGenreCommand
+            {
+                GenreName = genreName,
+                GenreType = genreType
+            };
+            // Act
+            var handler = new CreateGenreCommandHandler(_context.Object);
+            var result = await handler.Handle(command, CancellationToken.None);
+            // Assert
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.ShouldBeOfType<Guid>();
+        }
+
+        /// <summary>
+        /// Missing genre name should return a failure result
+        /// <see cref="CreateGenreCommand"/>
+        /// </summary>
+        [Fact]
+        public async Task MissingGenreNameShouldReturnFailureResult()
+        {
+            // Arrange
+            var genreName = string.Empty;
+            var genreType = "Test Type";
+            var command = new CreateGenreCommand
+            {
+                GenreName = genreName,
+                GenreType = genreType
+            };
+            // Act
+            var handler = new CreateGenreCommandHandler(_context.Object);
+            var result = await handler.Handle(command, CancellationToken.None);
+            // Assert
+            result.IsFailure.ShouldBeTrue();
+            result.Error.Type.ShouldBe(ErrorType.Validation);
+            result.Error.Code.ShouldBe("GenreName.Missing");
+        }
+
+        /// <summary>
+        /// missing genre type should return a failure result
+        /// <see cref="CreateGenreCommand"/>
+        /// </summary>
+        [Fact]
+        public async Task MissingGenreTypeShouldReturnFailureResult()
+        {
+            // Arrange
+            var genreName = "Test Genre";
+            var genreType = string.Empty;
+            var command = new CreateGenreCommand
+            {
+                GenreName = genreName,
+                GenreType = genreType
+            };
+            // Act
+            var handler = new CreateGenreCommandHandler(_context.Object);
+            var result = await handler.Handle(command, CancellationToken.None);
+            // Assert
+            result.IsFailure.ShouldBeTrue();
+            result.Error.Type.ShouldBe(ErrorType.Validation);
+            result.Error.Code.ShouldBe("GenreType.Missing");
+        }
+    }
+}
