a5772470f5a4bec1104fa9529ffcdbed9f9db0eb
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-04-17 22:15:03 +0200

    Remove Inactivate method

--- src/Domain/Models/Common/Tag.cs
+++ src/Domain/Models/Common/Tag.cs
@@ -43,11 +43,5 @@ namespace Domain.Models.Common
             UpdatedAt = DateTime.UtcNow;
             return Result.Success();
         }
-
-        public void Inactivate()
-        {
-            IsActive = false;
-            UpdatedAt = DateTime.UtcNow;
-        }
     }
 }
