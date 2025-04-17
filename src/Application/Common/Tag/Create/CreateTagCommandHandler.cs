a9749af1a5d9311f1c2d8af27a9b305ed2c9026a
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-04-17 22:02:30 +0200

    update Tag command handlers and test to use new interface

--- src/Application/Common/Tag/Create/CreateTagCommandHandler.cs
+++ src/Application/Common/Tag/Create/CreateTagCommandHandler.cs
@@ -5,7 +5,7 @@ using SharedKernel;
 
 namespace Application.Common
 {
-    internal sealed class CreateTagCommandHandler(IApplicationDbContext context) : ICommandHandler<CreateTagCommand, Guid>
+    internal sealed class CreateTagCommandHandler(IUnitOfWork context) : ICommandHandler<CreateTagCommand, Guid>
     {
         public async Task<Result<Guid>> Handle(CreateTagCommand request, CancellationToken cancellationToken)
         {
@@ -15,9 +15,12 @@ namespace Application.Common
                 return Result.Failure<Guid>(new Error(tagResult.Error.Code, tagResult.Error.Message, tagResult.Error.Type));
             }
 
-            context.Tags.Add(tagResult.Value);
-            await context.SaveChangesAsync(cancellationToken);
-
+            await context.TagRepository.AddAsync(tagResult.Value);
+            int result = await context.SaveChangesAsync(cancellationToken);
+            if(result == 0)
+            {
+                return Result.Failure<Guid>(new Error("Tag.CreateFailed", "Failed to create tag.", ErrorType.Problem));
+            }
             return Result.Success(tagResult.Value.Id);
         }
     }
--- src/Application/Common/Tag/Delete/DeleteTagCommandHandler.cs
+++ src/Application/Common/Tag/Delete/DeleteTagCommandHandler.cs
@@ -4,17 +4,21 @@ using SharedKernel;
 
 namespace Application.Common
 {
-    internal sealed class DeleteTagCommandHandler(IApplicationDbContext context) : ICommandHandler<DeleteTagCommand>
+    internal sealed class DeleteTagCommandHandler(IUnitOfWork context) : ICommandHandler<DeleteTagCommand>
     {
         public async Task<Result> Handle(DeleteTagCommand request, CancellationToken cancellationToken)
         {
-            var tag = await context.Tags.FindAsync(request.TagId, cancellationToken);
+            var tag = await context.TagRepository.GetByIdAsync(request.TagId);
             if (tag == null)
             {
                 return Result.Failure(new Error("Tag.NotFound", $"Tag with {request.TagId} ID is not found.", ErrorType.NotFound));
             }
             tag.Inactivate();
-            await context.SaveChangesAsync(cancellationToken);
+            int result = await context.SaveChangesAsync(cancellationToken);
+            if (result == 0)
+            {
+                return Result.Failure(new Error("Tag.DeleteFailed", "Failed to delete tag.", ErrorType.Problem));
+            }
             return Result.Success();
         }
     }
