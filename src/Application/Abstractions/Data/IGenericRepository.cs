445fab4592c88e6e1c205b6b2524772ccc69d309
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-04-17 21:42:41 +0200

    update interface

--- src/Application/Abstractions/Data/IGenericRepository.cs
+++ src/Application/Abstractions/Data/IGenericRepository.cs
@@ -2,7 +2,7 @@
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> GetByIdAsync(Guid id);
+        Task<T?> GetByIdAsync(Guid id);
         Task<IReadOnlyList<T>> GetAllAsync(bool includeInactive = false);
         Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity);
