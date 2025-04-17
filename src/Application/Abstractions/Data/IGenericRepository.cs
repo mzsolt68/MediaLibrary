4c497f7fe14ba39f65080780b87b34049d6d93dd
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-04-17 20:41:15 +0200

    Add repository and unit of work interfaces

---
+++ src/Application/Abstractions/Data/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Abstractions.Data
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> GetAllAsync(bool includeInactive = false);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ src/Application/Abstractions/Data/ITagRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Models.Common;
+
+namespace Application.Abstractions.Data
+{
+    public interface ITagRepository : IGenericRepository<Tag>
+    {
+    }
+}
