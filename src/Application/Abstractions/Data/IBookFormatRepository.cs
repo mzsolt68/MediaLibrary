301c9726a0c00014870f0a00beb36156328ef4f9
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-04-18 21:25:49 +0200

    add new repositories

---
+++ src/Application/Abstractions/Data/IBookFormatRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Models.Books;
+
+namespace Application.Abstractions.Data
+{
+    public interface IBookFormatRepository : IGenericRepository<BookFormat>
+    {
+    }
+}
--- src/Application/Abstractions/Data/IUnitOfWork.cs
+++ src/Application/Abstractions/Data/IUnitOfWork.cs
@@ -5,6 +5,9 @@
         IGenreRepository GenreRepository { get; }
         ITagRepository TagRepository { get; }
         ILanguageRepository LanguageRepository { get; }
+        IBookRepository BookRepository { get; }
+        IBookFormatRepository BookFormatRepository { get; }
+        IAuthorRepository AuthorRepository { get; }
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
