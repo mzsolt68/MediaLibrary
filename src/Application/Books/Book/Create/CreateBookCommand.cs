d415a7fc21c9121df1de65b66aab98668c4d418d
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-04-18 21:33:52 +0200

    add create book command, handler and tests

---
+++ src/Application/Books/Book/Create/CreateBookCommand.cs
@@ -0,0 +1,17 @@
+using Application.Abstractions.Messaging;
+
+namespace Application.Books
+{
+    public sealed record CreateBookCommand : ICommand<Guid>
+    {
+        public string BookTitle { get; set; }
+        public string Edition { get; set; }
+        public Guid PublisherID { get; set; }
+        public string PublishYear { get; set; }
+        public string ISBN { get; set; }
+        public Guid LanguageID { get; set; }
+        public ICollection<Guid> AuthorIDs { get; set; }
+        public ICollection<Guid> FormatIDs { get; set; }
+        public ICollection<Guid> TagIDs { get; set; }
+    }
+}
