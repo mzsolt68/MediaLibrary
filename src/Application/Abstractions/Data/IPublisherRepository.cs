5b2b12821248df69086b042d964775f44a17829b
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-04-19 18:59:18 +0200

    add new interface to work with Publisher object

---
+++ src/Application/Abstractions/Data/IPublisherRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models.Books;
+
+namespace Application.Abstractions.Data
+{
+    public interface IPublisherRepository : IGenericRepository<Publisher>
+    {
+        Task DeleteBooks(Guid publisherId);
+        Task<IReadOnlyCollection<Book>> GetBooks(Guid publisherId);
+    }
+}
