b9d5e88de39d8dfa9d25d7973231e804a995df4b
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-04-21 20:26:41 +0200

    Add query, queryhandler and test cases to get a list of tags

---
+++ tests/Application.UnitTests/Common/Tag/GetTagsQueryHandlerTests.cs
@@ -0,0 +1,65 @@
+using Application.Abstractions.Data;
+using Application.Common;
+using Application.Dto.Common;
+using Domain.Models.Common;
+using Moq;
+using Shouldly;
+
+namespace Application.UnitTests.Common
+{
+    public class GetTagsQueryHandlerTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+        private readonly Mock<ITagRepository> _tagRepository;
+
+        public GetTagsQueryHandlerTests()
+        {
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _tagRepository = new Mock<ITagRepository>();
+            _unitOfWork.Setup(x => x.TagRepository).Returns(_tagRepository.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnListOfTagDTOs_WhenTagsExist()
+        {
+            // Arrange
+            var tags = new List<Tag>
+            {
+                Tag.Create("Horror").Value,
+                Tag.Create("Comedy").Value
+            };
+            _tagRepository.Setup(x => x.GetAllAsync(false)).ReturnsAsync(tags);
+
+            var query = new GetTagsQuery();
+            var handler = new GetTagsQueryHandler(_unitOfWork.Object);
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.ShouldNotBeNull();
+            result.Value.Count.ShouldBe(2);
+            result.Value.GetType().ShouldBe(typeof(List<TagDTO>));
+            result.Value[0].TagName.ShouldBe("Horror");
+            result.Value[1].TagName.ShouldBe("Comedy");
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnFailure_WhenNoTagsExist()
+        {
+            // Arrange
+            _tagRepository.Setup(x => x.GetAllAsync(false)).ReturnsAsync(new List<Tag>());
+
+            var query = new GetTagsQuery();
+            var handler = new GetTagsQueryHandler(_unitOfWork.Object);
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.IsFailure.ShouldBeTrue();
+            result.Error.Code.ShouldBe("Tags.NotFound");
+        }
+    }
+}
