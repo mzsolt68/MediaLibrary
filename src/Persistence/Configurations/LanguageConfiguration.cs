0743a18da1420313b4350f4ea6aa17dc213f578a
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-04-23 21:54:07 +0200

    Add entity configuration classes

---
+++ src/Persistence/Configurations/LanguageConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations
+{
+    internal class LanguageConfiguration : IEntityTypeConfiguration<Language>
+    {
+        public void Configure(EntityTypeBuilder<Language> builder)
+        {
+            builder.ToTable("Languages");
+            builder.HasKey(l => l.Id);
+            builder.Property(l => l.LanguageName).IsRequired().HasMaxLength(50);
+        }
+    }
+}
---
+++ src/Persistence/Configurations/TagConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Models.Books;
+using Domain.Models.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations
+{
+    internal class TagConfiguration : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.ToTable("Tags");
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.TagName)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
