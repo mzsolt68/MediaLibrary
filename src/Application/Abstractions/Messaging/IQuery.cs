114f903c35f2da607225ef2218b67c6ff2fb2c32
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-04-28 20:20:35 +0200

    Add documentation to Messaging abstraction

--- src/Application/Abstractions/Messaging/IQuery.cs
+++ src/Application/Abstractions/Messaging/IQuery.cs
@@ -3,6 +3,10 @@ using SharedKernel;
 
 namespace Application.Abstractions.Messaging
 {
+    /// <summary>
+    /// Represents a query that returns a result of type <typeparamref name="TResponse"/>.
+    /// </summary>
+    /// <typeparam name="TResponse">The type of the response returned by the query.</typeparam>
     public interface IQuery<TResponse> : IRequest<Result<TResponse>>
     {
     }
