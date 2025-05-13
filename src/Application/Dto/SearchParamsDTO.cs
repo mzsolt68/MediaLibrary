d487039707acee3dedbbd2aec3db34ea4e3b6406
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-05-13 22:28:59 +0200

    Add new DTO to send search parameters sent from the client.

---
+++ src/Application/Dto/SearchParamsDTO.cs
@@ -0,0 +1,54 @@
+﻿using SharedKernel;
+
+namespace Application.Dto
+{
+    /// <summary>
+    /// Data Transfer Object for search parameters, including pagination, filtering, and sorting options.
+    /// </summary>
+    public class SearchParamsDTO
+    {
+        /// <summary>
+        /// Gets or sets the page number for pagination.
+        /// The default value is 1.
+        /// </summary>
+        public int PageNumber { get; set; } = 1;
+
+        /// <summary>
+        /// Gets or sets the page size for pagination.
+        /// The default value is 10.
+        /// </summary>
+        public int PageSize { get; set; } = 10;
+
+        /// <summary>
+        /// Gets or sets the collection of search parameters to filter results.
+        /// </summary>
+        public ICollection<SearchParam> SearchParams { get; set; } = [];
+
+        /// <summary>
+        /// Gets or sets the sort order for the results.
+        /// </summary>
+        public string? SortOrder { get; set; }
+    }
+
+    /// <summary>
+    /// Represents a single search parameter used for filtering results.
+    /// </summary>
+    public class SearchParam
+    {
+        /// <summary>
+        /// Gets or sets the name of the property to search.
+        /// </summary>
+        public string PropertyName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the value to search for.
+        /// </summary>
+        public string Value { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the match type for the search.
+        /// The default value is <see cref="SearchType.Contains"/>.
+        /// </summary>
+        public SearchType MatchType { get; set; } = SearchType.Contains;
+    }
+}
---
+++ src/SharedKernel/SearchType.cs
@@ -0,0 +1,19 @@
+﻿namespace SharedKernel
+{
+    /// <summary>
+    /// SearchType enumeration defines various types of search operations.
+    /// </summary>
+    public enum SearchType
+    {
+        Exact = 0,
+        Contains = 1,
+        StartsWith = 2,
+        EndsWith = 3,
+        GreaterThan = 4,
+        GreaterThanOrEqual = 5,
+        LessThan = 6,
+        LessThanOrEqual = 7,
+        NotEqual = 8,
+        Casesensitive = 9
+    }
+}
