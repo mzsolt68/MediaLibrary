24f9f9a3c2e736bea8a1e22bfdeca6c7e0a57da5
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-05-14 22:05:15 +0200

    Update the GetTags qzuery handler to take a DTO object as a collection of search parameters. Update the repository calls.

--- src/Application/Common/Tag/GetAll/GetTagsQuery.cs
+++ src/Application/Common/Tag/GetAll/GetTagsQuery.cs
@@ -1,11 +1,11 @@
 using Application.Abstractions.Messaging;
+using Application.Dto;
 using Application.Dto.Common;
-using System.Linq.Expressions;
 
 namespace Application.Common
 {
     /// <summary>
     /// Represents a query to retrieve all tags.
     /// </summary>
-    public sealed record GetTagsQuery<T>(Expression<Func<T, bool>> Predicate) : IQuery<List<TagDTO>>;
+    public sealed record GetTagsQuery<T>(SearchParamsDTO SearchParams) : IQuery<List<TagDTO>>;
 }
--- src/Application/Common/Tag/GetAll/GetTagsQueryHandler.cs
+++ src/Application/Common/Tag/GetAll/GetTagsQueryHandler.cs
@@ -1,9 +1,13 @@
-using Application.Abstractions.Messaging;
-using Application.Dto.Common;
-using SharedKernel;
 using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Application.Dto;
+using Application.Dto.Common;
 using Application.Dto.ConvertObjects;
 using Domain.Models.Common;
+using Microsoft.EntityFrameworkCore;
+using SharedKernel;
+using SharedKernel.Extensions;
+using System.Linq.Expressions;
 
 namespace Application.Common
 {
@@ -24,7 +28,19 @@ namespace Application.Common
         /// </returns>
         public async Task<Result<List<TagDTO>>> Handle(GetTagsQuery<Tag> request, CancellationToken cancellationToken)
         {
-            var tags = await context.TagRepository.GetAllAsync(request.Predicate);
+            IQueryable<Tag> tagsQuery;
+            int skip = (request.SearchParams.PageNumber - 1) * request.SearchParams.PageSize;
+
+            if (request.SearchParams.SearchParams.Count == 0)
+            {
+                tagsQuery = context.TagRepository.GetAll();
+            }
+            else
+            {
+                tagsQuery = context.TagRepository.GetAll(CreateFilter(request.SearchParams));
+            }
+
+            IReadOnlyList<Tag> tags = await tagsQuery.Skip(skip).Take(request.SearchParams.PageSize).ToListAsync(cancellationToken);
 
             if (tags == null || !tags.Any())
             {
@@ -34,5 +50,32 @@ namespace Application.Common
             var tagDtos = tags.Select(tag => tag.AsTagDTO()).ToList();
             return Result.Success(tagDtos);
         }
+
+        private Expression<Func<Tag, bool>> CreateFilter(SearchParamsDTO searchParams)
+        {
+            Expression<Func<Tag, bool>> predicate = genre => genre.IsActive;
+            foreach (var filter in searchParams.SearchParams)
+            {
+                Expression<Func<Tag, bool>> filterExpr = filter.MatchType switch
+                {
+                    SearchType.Contains => tag =>
+                        (tag.GetPropertyValue(filter.PropertyName)!.ToString() ?? string.Empty)
+                            .Contains(filter.Value),
+                    SearchType.Exact => tag =>
+                        (tag.GetPropertyValue(filter.PropertyName)!.ToString() ?? string.Empty)
+                            == filter.Value,
+                    SearchType.StartsWith => tag =>
+                        (tag.GetPropertyValue(filter.PropertyName)!.ToString() ?? string.Empty)
+                            .StartsWith(filter.Value),
+                    SearchType.EndsWith => tag =>
+                        (tag.GetPropertyValue(filter.PropertyName)!.ToString() ?? string.Empty)
+                            .EndsWith(filter.Value),
+                    _ => tag => true
+                };
+                predicate = predicate.AndAlso(filterExpr);
+            }
+            return predicate;
+        }
+
     }
 }
