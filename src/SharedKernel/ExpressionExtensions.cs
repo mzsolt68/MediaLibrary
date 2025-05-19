0709205f471fb014a4b092b94026d50f2fa7aaf7
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-05-19 23:58:42 +0200

    Modify the Expression extension method

--- src/SharedKernel/ExpressionExtensions.cs
+++ src/SharedKernel/ExpressionExtensions.cs
@@ -18,10 +18,30 @@ namespace SharedKernel.Extensions
             Expression<Func<T, bool>> expr2)
         {
             var parameter = Expression.Parameter(typeof(T));
-            var body = Expression.AndAlso(
-                Expression.Invoke(expr1, parameter),
-                Expression.Invoke(expr2, parameter));
-            return Expression.Lambda<Func<T, bool>>(body, parameter);
+            var leftVisitor = new ReplaceParameterVisitor(expr1.Parameters[0], parameter);
+            var left = leftVisitor.Visit(expr1.Body);
+            var rightVisitor = new ReplaceParameterVisitor(expr2.Parameters[0], parameter);
+            var right = rightVisitor.Visit(expr2.Body);
+
+            return Expression.Lambda<Func<T, bool>>(
+                Expression.AndAlso(left!, right!),parameter);
+        }
+    }
+
+    class ReplaceParameterVisitor : ExpressionVisitor
+    {
+        private readonly ParameterExpression _oldParameter;
+        private readonly ParameterExpression _newParameter;
+
+        public ReplaceParameterVisitor(ParameterExpression oldParameter, ParameterExpression newParameter)
+        {
+            _oldParameter = oldParameter;
+            _newParameter = newParameter;
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return node == _oldParameter ? _newParameter : base.VisitParameter(node);
         }
     }
 }
