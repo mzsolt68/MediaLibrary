92bc015b7a000a50e87ff2d5efe28ca24e2c14ad
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-05-24 22:10:45 +0200

    Add query and queryhandler to get Author from database by ID. Add unit tests.

---
+++ src/Application/Books/Author/GetById/GetAuthorByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Dto.Books;
+
+namespace Application.Books
+{
+    public sealed record GetAuthorByIdQuery(Guid AuthorId) : IQuery<BookAuthorDTO>;
+}
---
+++ src/Application/Books/Author/GetById/GetAuthorByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Application.Dto.Books;
+using Application.Dto.ConvertObjects;
+using SharedKernel;
+
+namespace Application.Books
+{
+    public sealed class GetAuthorByIdQueryHandler(IUnitOfWork context) : IQueryHandler<GetAuthorByIdQuery, BookAuthorDTO>
+    {
+        public async Task<Result<BookAuthorDTO>> Handle(GetAuthorByIdQuery request, CancellationToken cancellationToken)
+        {
+            var author = await context.AuthorRepository.GetByIdAsync(request.AuthorId, cancellationToken);
+            if (author == null)
+            {
+                return Result.Failure<BookAuthorDTO>(new Error("Author.NotFound", "The author was not found in the database.", ErrorType.NotFound));
+            }
+            return Result.Success(author.AsAuthorDTO());
+        }
+    }
+}
