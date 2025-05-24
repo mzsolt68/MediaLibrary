491fa0de19178228368282bd7ef4575b1c1dd7b3
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-05-25 00:36:57 +0200

    Add query and queryhandler to get a list of books (including authors) from the database. Add unit tests.

---
+++ src/Application/Books/Book/GetAll/GetBooksQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Dto;
+using Application.Dto.Books;
+
+namespace Application.Books
+{
+    /// <summary>
+    /// Represents a query to retrieve a list of books based on the specified search parameters.
+    /// </summary>
+    /// <param name="SearchParams">The search parameters used to filter the books. This parameter must not be <see langword="null"/>.</param>
+    public sealed record GetBooksQuery(SearchParamsDTO SearchParams) : IQuery<List<BookDTO>>;
+}
