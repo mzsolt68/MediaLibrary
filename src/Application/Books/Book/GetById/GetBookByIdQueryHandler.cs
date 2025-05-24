6415450f9473e7fb7bbfe6f5d4b50e41cfc66c1c
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-05-25 00:48:25 +0200

    Add query and query handler to get a book (with author) by ID from the database. Add unit tests.

---
+++ src/Application/Books/Book/GetById/GetBookByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Application.Abstractions.Data;
+using Application.Abstractions.Messaging;
+using Application.Dto.Books;
+using Application.Dto.ConvertObjects;
+using Domain.Models.Books;
+using SharedKernel;
+
+namespace Application.Books
+{
+    /// <summary>
+    /// Handles the query to retrieve a book by its unique identifier.
+    /// </summary>
+    /// <remarks>This query handler retrieves a book from the data source using the provided book ID.  If the
+    /// book is not found, a failure result is returned with an appropriate error message.</remarks>
+    /// <param name="context">The unit of work used to access the book repository.</param>
+    public sealed class GetBookByIdQueryHandler(IUnitOfWork context) : IQueryHandler<GetBookByIdQuery, BookDTO>
+    {
+        /// <summary>
+        /// Handles the retrieval of a book by its unique identifier.
+        /// </summary>
+        /// <param name="request">The query containing the unique identifier of the book to retrieve.</param>
+        /// <param name="cancellationToken">A token to monitor for cancellation requests.</param>
+        /// <returns>A <see cref="Result{T}"/> containing a <see cref="BookDTO"/> if the book is found; otherwise, a failure
+        /// result with an error indicating that the book was not found.</returns>
+        public async Task<Result<BookDTO>> Handle(GetBookByIdQuery request, CancellationToken cancellationToken)
+        {
+            Book? book = await context.BookRepository.GetByIdAsync(request.BookId, cancellationToken);
+            if(book is null)
+            {
+                return Result.Failure<BookDTO>(new Error(
+                    "Book.NotFound", $"The book with Id {request.BookId} was not found.", ErrorType.NotFound));
+            }
+            return Result.Success(book.AsBookDTO(true));
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Books/Book/GetBookByIdQueryHandlerTests.cs
@@ -0,0 +1,64 @@
+﻿using Application.Abstractions.Data;
+using Application.Books;
+using Application.Dto.Books;
+using Domain.Models.Books;
+using Moq;
+using SharedKernel;
+using Shouldly;
+
+namespace Application.UnitTests.Books
+{
+    public class GetBookByIdQueryHandlerTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+        private readonly Mock<IBookRepository> _bookRepository;
+
+        public GetBookByIdQueryHandlerTests()
+        {
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _bookRepository = new Mock<IBookRepository>();
+            _unitOfWork.Setup(x => x.BookRepository).Returns(_bookRepository.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnBookDTO_WhenBookExists()
+        {
+            // Arrange
+            var book = Book.Create("Title", "2nd", Guid.NewGuid(), "2000", "", Guid.NewGuid()).Value;
+            book.AddAuthor(Author.Create("Lastname", "Firstname", "").Value);
+            _bookRepository.Setup(x => x.GetByIdAsync(book.Id, CancellationToken.None)).ReturnsAsync(book);
+
+            var query = new GetBookByIdQuery(book.Id);
+            var handler = new GetBookByIdQueryHandler(_unitOfWork.Object);
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.IsSuccess.ShouldBeTrue();
+            result.Value.ShouldNotBeNull();
+            result.Value.ShouldBeOfType<BookDTO>();
+            result.Value.Authors.Count.ShouldBe(1);
+            result.Value.BookTitle.ShouldBe(book.BookTitle);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldReturnFailure_WhenBookDoesNotExist()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+            _bookRepository.Setup(x => x.GetByIdAsync(bookId, CancellationToken.None)).ReturnsAsync((Book?)null);
+
+            var query = new GetBookByIdQuery(bookId);
+            var handler = new GetBookByIdQueryHandler(_unitOfWork.Object);
+
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.IsFailure.ShouldBeTrue();
+            result.Error.Code.ShouldBe("Book.NotFound");
+            result.Error.Type.ShouldBe(ErrorType.NotFound);
+        }
+    }
+}
