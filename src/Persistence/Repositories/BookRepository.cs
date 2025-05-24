f1c1769654b88e2aed27985f0701cd5144c1dfdb
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-05-25 00:19:25 +0200

    Override the GetByIdAsync method of the Generic repository to get the book with Author information.

--- src/Persistence/Repositories/BookRepository.cs
+++ src/Persistence/Repositories/BookRepository.cs
@@ -71,5 +71,13 @@ namespace Persistence.Repositories
                 .Include(b => b.Language)
                 .FirstOrDefaultAsync(b => b.Id == bookId, cancellationToken: cancellationToken);
         }
+
+        public new async Task<Book?> GetByIdAsync(Guid id, CancellationToken cancellation = default)
+        {
+            return await _context.Books
+                .Include(b => b.Authors)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(b => b.Id == id, cancellationToken: cancellation);
+        }
     }
 }
