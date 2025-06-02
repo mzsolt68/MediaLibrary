08255b3e0a504c257c704f33378748734bfc5b37
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-06-02 23:54:31 +0200

    Add query and querahandler to get all BookFormats from the database. Add unit tests.

---
+++ src/Application/Books/BookFormat/GetAll/GetBookFormatsQuery.cs
@@ -0,0 +1,12 @@
+﻿using Application.Abstractions.Messaging;
+using Application.Dto;
+using Application.Dto.Books;
+
+namespace Application.Books
+{
+    /// <summary>
+    /// Represents a query to retrieve a list of book formats based on the specified search parameters.
+    /// </summary>
+    /// <param name="SearchParams">The search parameters used to filter the book formats. This parameter cannot be null.</param>
+    public sealed record GetBookFormatsQuery(SearchParamsDTO SearchParams) : IQuery<List<BookFormatDTO>>;
+}
