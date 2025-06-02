49a8cfe42a05f56cc197b8fcc0d7a361134f7b63
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2025-06-02 23:08:39 +0200

    Add new API controller to handle Book/Author entity

---
+++ src/Api/Controllers/Books/AuthorController.cs
@@ -0,0 +1,69 @@
+﻿using Application.Books;
+using Application.Common;
+using Application.Dto;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel.Extensions;
+
+namespace Api.Controllers.Books
+{
+    [Route("api/books/authors")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthorController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetAuthorById(Guid id)
+        {
+            var query = new GetAuthorByIdQuery(id);
+            var result = await _mediator.Send(query);
+            return result.IsSuccess ? Ok(result.Value) : BadRequest(result.Error);
+        }
+
+        [HttpPost("list")]
+        public async Task<IActionResult> GetAuthors([FromBody]SearchParamsDTO searchParams)
+        {
+            if(searchParams is null)
+            {
+                return BadRequest();
+            }
+            var query = new GetAuthorsQuery(searchParams);
+            var result = await _mediator.Send(query);
+            return result.IsSuccess ? Ok(result.Value) : BadRequest(result.Error);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAuthor([FromBody] CreateAuthorCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return result.IsSuccess ? CreatedAtAction(nameof(GetAuthorById), new { id = result.Value }, result.Value) : BadRequest(result.Error);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateAuthor(Guid id, [FromBody]UpdateAuthorCommand command)
+        {
+            if(id != command.AuthorId)
+            {  
+                return BadRequest("ID mismatch"); 
+            }
+            var result = await _mediator.Send(command);
+            return result.IsSuccess ? NoContent() : BadRequest(result.Error);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteAuthor(Guid id)
+        {
+            var command = new DeleteAuthorCommand(id);
+            var result = await _mediator.Send(command);
+            return result.IsSuccess ? NoContent() : BadRequest(result.Error);
+        }
+
+    }
+}
